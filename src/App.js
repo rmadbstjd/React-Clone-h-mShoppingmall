d812d84c94cf14ccbf01cbe55f4408e07a09824d
rmadbstjd <rmadbstjd@naver.com>
2022-11-03 12:44:43 +0900

    react-router-dom 라이브러리를 설치 후, page 폴더를 만들어서 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <ProductAll />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/product/:id",
+      element: <ProductDetail />,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
